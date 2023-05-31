b1efb4831a252111e6371efafb809f7ab630f632
SamuelEBG <95290084+SamuelEBG@users.noreply.github.com>
2023-05-31 13:49:24 +0200

    added category endpoint

--- src/main/java/no/kristiania/collectthemunch/entities/Category.java
+++ src/main/java/no/kristiania/collectthemunch/entities/Category.java
@@ -1,5 +1,12 @@
 package no.kristiania.collectthemunch.entities;
 
+import java.util.Arrays;
+import java.util.List;
+
 public enum Category {
-    PARTY, EXHIBITION, KIDS, FAMILY, NEW, GAMES
+    PARTY, EXHIBITION, KIDS, FAMILY, NEW, GAMES;
+
+    public static List<Category> getAllValues() {
+        return Arrays.asList(Category.values());
+    }
 }
