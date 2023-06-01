5700a8b9d52358e8bd70e4cad91168a7836dff62
Marcus <60523971+Simpada@users.noreply.github.com>
2023-06-01 10:02:38 +0200

    Fixed generate user, event, and reviews

--- src/test/java/no/kristiania/collectthemunch/SampleData.java
+++ src/test/java/no/kristiania/collectthemunch/SampleData.java
@@ -47,9 +47,45 @@ public class SampleData {
                 "fdawdngoe"
         )) + "@munch.com";
 
-
-        // Also need to randomize preferences
         List<Category> preferences = new ArrayList<>();
+        int preferenceCount = random.nextInt(4) + 1;
+        for (int i = 0; i < preferenceCount; i++) {
+
+            int category = random.nextInt(6);
+
+            switch (category) {
+                case 0 -> {
+                    if (!preferences.contains(Category.PARTY)) {
+                        preferences.add(Category.PARTY);
+                    }
+                }
+                case 1 -> {
+                    if (!preferences.contains(Category.EXHIBITION)) {
+                        preferences.add(Category.EXHIBITION);
+                    }
+                }
+                case 2 -> {
+                    if (!preferences.contains(Category.KIDS)) {
+                        preferences.add(Category.KIDS);
+                    }
+                }
+                case 3 -> {
+                    if (!preferences.contains(Category.FAMILY)) {
+                        preferences.add(Category.FAMILY);
+                    }
+                }
+                case 4 -> {
+                    if (!preferences.contains(Category.NEW)) {
+                        preferences.add(Category.NEW);
+                    }
+                }
+                case 5 -> {
+                    if (!preferences.contains(Category.GAMES)) {
+                        preferences.add(Category.GAMES);
+                    }
+                }
+            }
+        }
 
         var user = new User();
         user.setUserName(name);
@@ -63,14 +99,89 @@ public class SampleData {
 
     public static Event sampleEvent() {
 
+        var name = (pickOne(
+                "Cool Guy Event",
+                "Painting Hunt",
+                "Finger Paint Competition",
+                "Munch Freestyle rap",
+                "Party Time"
+        )) + random.nextInt(10000000);
+
+        var description = (pickOne(
+                "Fun event at MUNCH",
+                "Win great rewards",
+                "Have the time of your life",
+                "Bring all your friends",
+                "Express yourself",
+                "Create great memories"
+        ));
+
+        List<Category> categories = new ArrayList<>();
+        int categoryCount = random.nextInt(4) + 1;
+        for (int i = 0; i < categoryCount; i++) {
+
+            int category = random.nextInt(6);
+
+            switch (category) {
+                case 0 -> {
+                    if (!categories.contains(Category.PARTY)) {
+                        categories.add(Category.PARTY);
+                    }
+                }
+                case 1 -> {
+                    if (!categories.contains(Category.EXHIBITION)) {
+                        categories.add(Category.EXHIBITION);
+                    }
+                }
+                case 2 -> {
+                    if (!categories.contains(Category.KIDS)) {
+                        categories.add(Category.KIDS);
+                    }
+                }
+                case 3 -> {
+                    if (!categories.contains(Category.FAMILY)) {
+                        categories.add(Category.FAMILY);
+                    }
+                }
+                case 4 -> {
+                    if (!categories.contains(Category.NEW)) {
+                        categories.add(Category.NEW);
+                    }
+                }
+                case 5 -> {
+                    if (!categories.contains(Category.GAMES)) {
+                        categories.add(Category.GAMES);
+                    }
+                }
+            }
+        }
+
         var event = new Event();
+
+        //event.setName(name);
+        event.setDescription(description);
+        event.setCategories(categories);
 
         return event;
     }
 
     public static Review sampleReview() {
 
+        var reviewText = (pickOne(
+                "Had the best time",
+                "Not enough modern art",
+                "The staff were great",
+                "Love it",
+                "Fun",
+                "Woof woof bark bark"
+        ));
+
+        var numOfStars = random.nextInt(5) + 1;
+
         var review = new Review();
+
+        review.setReviewText(reviewText);
+        review.setNumOfStars(numOfStars);
 
         return review;
     }
@@ -81,7 +192,6 @@ public class SampleData {
 
         return painting;
     }
-
 
     private static String pickOne(String... alternatives) {
         return alternatives[random.nextInt(alternatives.length)];
