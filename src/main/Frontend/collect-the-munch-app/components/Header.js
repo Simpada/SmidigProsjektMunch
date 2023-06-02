d8e3748802c000bc8fb0bbd8409811d894e7a26d
Jonas Andersen <94622735+jonasanders1@users.noreply.github.com>
2023-06-02 14:20:47 +0200

    Menu-button
    Design adjusted

--- src/main/Frontend/collect-the-munch-app/components/Header.js
+++ src/main/Frontend/collect-the-munch-app/components/Header.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react'
-import { Text, StyleSheet, SafeAreaView, TouchableOpacity } from 'react-native'
+import { Text, StyleSheet, SafeAreaView, TouchableOpacity, View, Image } from 'react-native'
 import { colors } from '../Styles/theme'
 import MenuModal from './MenuModal'
-
+import menuIcon from '../assets/Images/menu.png'
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
@@ -18,9 +18,9 @@ const Header = () => {
     <SafeAreaView style={styles.headerContainer}>
       <Text style={styles.headerText}>Collect the MUNCH</Text>
       <TouchableOpacity style={styles.menuBtn} onPress={handleMenuClick}>
-        <Text style={styles.menuBtnText}>
-          Menu
-        </Text>   
+        <View style={styles.menuBtnText}>
+          <Image source={menuIcon} style={styles.menuIcon} />
+        </View>   
       </TouchableOpacity>
       <MenuModal visible={isMenuOpen} closeMenu={closeMenu} />
     </SafeAreaView>
@@ -48,13 +48,22 @@ const styles = StyleSheet.create({
     paddingHorizontal: 10,
     fontFamily: "GirottMunch-BoldBackslant"
   },
+  menuIcon: {
+    height: 30,
+    width: 30,
+    backgroundColor: colors.red,
+    padding: 10,
+    
+  },
   menuBtn: {
     position: "absolute",
-    bottom: "35%",
+    bottom: "20%",
     right: 15,
   },
   menuBtnText: {
-    color: colors.white,
-    fontSize: 20
+    backgroundColor: colors.red,
+    fontSize: 20,
+    padding: 5,
+    borderRadius: 5
   }
 })
--- src/main/Frontend/collect-the-munch-app/components/MenuModal.js
+++ src/main/Frontend/collect-the-munch-app/components/MenuModal.js
@@ -37,15 +37,16 @@ const MenuModal = ({ visible, closeMenu }) => {
 const styles = StyleSheet.create({
   modalContainer: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  modalContent: {
+    justifyContent: 'flex-start',
+    alignItems: 'flex-end',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+},
+modalContent: {
     backgroundColor: colors.navy,
     padding: 20,
-    marginTop: 50,
-    width: '100%',
-    height: "95%",
+    marginTop: 100,
+    width: '80%',
+    height: "60%",
     alignItems: 'center',
   },
   closeButton: {
