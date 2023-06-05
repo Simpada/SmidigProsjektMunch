fff03039b75ae00f52b0ee62c6b549e170239f8a
Mjerki <91531326+Mjerki@users.noreply.github.com>
2023-06-05 15:22:59 +0200

    Leaderboard header added

--- src/main/Frontend/collect-the-munch-app/screens/LeaderboardScreen.js
+++ src/main/Frontend/collect-the-munch-app/screens/LeaderboardScreen.js
@@ -1,92 +1,139 @@
-import { useEffect, useState } from 'react';
-import { Text, View, StyleSheet, ScrollView, Pressable, Alert } from 'react-native'
+import React, { useEffect, useState } from 'react';
+import { Text, View, StyleSheet, ScrollView, Image } from 'react-native';
 import { colors } from '../Styles/theme';
-const LeaderboardScreen = () => {
 
-  const [leaderboard, setLeaderBoard] = useState([])
+const LeaderboardScreen = () => {
+  const [leaderboard, setLeaderBoard] = useState([]);
 
   useEffect(() => {
-      const initialLeaderboard = [
-        {id: 1, fullName:"Jonas Andersen", userName: "Jønna", points: 2000 },
-        {id: 2, fullName:"Sander Sanstrød", userName: "Sand", points: 1800 },
-        {id: 3, fullName:"Samuel something", userName: "The", points: 1500 },
-      ]
+    const initialLeaderboard = [
+      { id: 1, fullName: 'Jonas Andersen', userName: 'Jønna', points: 2000, profileImage: require('../assets/profile1.png') },
+      { id: 2, fullName: 'Sander Sanstrød', userName: 'Sand', points: 1800, profileImage: require('../assets/profile2.png') },
+      { id: 3, fullName: 'Samuel something', userName: 'The', points: 1500, profileImage: require('../assets/profile3.png') },
+    ];
 
-      setLeaderBoard(initialLeaderboard)
-  }, [])
+    setLeaderBoard(initialLeaderboard);
+  }, []);
 
-
-
-    return  (
-      <View style={styles.container}>
-        <View style={styles.categorycontainer}>
-          <TouchableOpacity style={styles.category}
-          >
-            <Text style={styles.categoryText}>Weekly</Text>
-          </TouchableOpacity>
-          <Pressable style={styles.category}>
-            <Text style={styles.categoryText}>Monthly</Text>
-          </Pressable>
-          <Pressable style={styles.category}>
-            <Text style={styles.categoryText}>All-time</Text>
-          </Pressable>
+  return (
+    <View style={styles.container}>
+      <View style={styles.categorycontainer}>
+        <View style={styles.category}>
+          <Text style={styles.categoryText}>Weekly</Text>
         </View>
-
-       <ScrollView>
-      <View style={styles.table}>
-        <View style={styles.tableHeader}>
-          <Text style={styles.headerIdText}>ID</Text>
-          <Text style={styles.headerText}>Full Name</Text>
-          <Text style={styles.headerText}>Name</Text>
-          <Text style={styles.headerText}>Points</Text>
+        <View style={styles.category}>
+          <Text style={styles.categoryText}>Monthly</Text>
         </View>
-        {leaderboard.map((item, index) => (
-          <View
-            style={
-              [
-                styles.tableRow, index <= 0 && styles.topOneRow,
-                styles.tableRow, index <= 1 && index >=1 && styles.topTwoRow,
-                styles.tableRow, index <= 2 && index >=2 && styles.topThreeRow,
-                styles.tableRow, index > 2 && styles.restPos,
-
-              ]
-            }
-            key={item.id}
-          >
-            <Text style={styles.idCell}>{item.id}</Text>
-            <Text style={styles.cell}>{item.fullName}</Text>
-            <Text style={styles.cell}>{item.userName}</Text>
-            <Text style={styles.cell}>{item.points}</Text>
-          </View>
-        ))}
+        <View style={styles.category}>
+          <Text style={styles.categoryText}>All-time</Text>
+        </View>
       </View>
-    </ScrollView>
+
+      <View style={styles.headerContainer}>
+        <Text style={styles.headerText}>Top 3 Winners</Text>
+        <ScrollView horizontal>
+          {leaderboard.slice(0, 3).map((item) => (
+            <View style={styles.winnerContainer} key={item.id}>
+              <View style={styles.profileImageContainer}>
+                <Image source={item.profileImage} style={styles.profileImage} />
+              </View>
+              <Text style={styles.winnerName}>{item.fullName}</Text>
+              <Text style={styles.winnerPoints}>{item.points} points</Text>
+            </View>
+          ))}
+        </ScrollView>
+      </View>
+
+      <ScrollView>
+        <View style={styles.table}>
+          <View style={styles.tableHeader}>
+            <Text style={styles.headerIdText}>ID</Text>
+            <Text style={styles.headerText}>Full Name</Text>
+            <Text style={styles.headerText}>Name</Text>
+            <Text style={styles.headerText}>Points</Text>
+          </View>
+          {leaderboard.map((item, index) => (
+            <View
+              style={[
+                styles.tableRow,
+                index === 0 && styles.topOneRow,
+                index === 1 && styles.topTwoRow,
+                index === 2 && styles.topThreeRow,
+              ]}
+              key={item.id}
+            >
+              <View style={styles.circle}>
+                <Text style={styles.circleText}>{item.id}</Text>
+              </View>
+              <Text style={styles.cell}>{item.fullName}</Text>
+              <Text style={styles.cell}>{item.userName}</Text>
+              <Text style={styles.cell}>{item.points}</Text>
+            </View>
+          ))}
+        </View>
+      </ScrollView>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
     paddingHorizontal: 20,
-    backgroundColor: colors.navy
+    backgroundColor: colors.navy,
   },
-   categorycontainer:{
+  categorycontainer: {
     backgroundColor: colors.black,
-    width: "100%",
+    width: '100%',
     marginVertical: 30,
     padding: 10,
     borderRadius: 20,
-    justifyContent:"center",
-    gap:50,
-    flexDirection:"row"
+    justifyContent: 'center',
+    flexDirection: 'row',
   },
-  category:{
+  category: {
     padding: 5,
   },
-
   categoryText: {
-    fontWeight: "bold",
-    color: "white"
+    fontWeight: 'bold',
+    color: 'white',
+  },
+  headerContainer: {
+    marginTop: 20,
+    marginBottom: 10,
+    alignItems: 'center',
+  },
+  headerText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: colors.white,
+    marginBottom: 10,
+  },
+  winnerContainer: {
+    alignItems: 'center',
+    marginRight: 10,
+  },
+  profileImageContainer: {
+    width: 80,
+    height: 80,
+    borderRadius: 40,
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  profileImage: {
+    width: 70,
+    height: 70,
+    borderRadius: 35,
+  },
+  winnerName: {
+    color: colors.white,
+    fontWeight: 'bold',
+    marginTop: 5,
+  },
+  winnerPoints: {
+    color: colors.white,
+    fontWeight: 'bold',
   },
   table: {
     borderColor: 'black',
@@ -96,8 +143,8 @@ const styles = StyleSheet.create({
     backgroundColor: colors.navy,
     padding: 10,
   },
-  headerIdText:{
-    flex:1,
+  headerIdText: {
+    flex: 1,
     color: colors.white,
     fontWeight: 'bold',
   },
@@ -112,34 +159,38 @@ const styles = StyleSheet.create({
     borderBottomWidth: 1,
     borderColor: 'gray',
     padding: 10,
+    alignItems: 'center',
   },
   topOneRow: {
-    backgroundColor: "green",
-    color: colors.white 
+    backgroundColor: 'green',
+    color: colors.white,
   },
   topTwoRow: {
-    backgroundColor: "yellow",
-    color: colors.white 
+    backgroundColor: 'yellow',
+    color: colors.white,
   },
-  topThreeRow:{
-    backgroundColor: "orange",
-    color: colors.white 
+  topThreeRow: {
+    backgroundColor: 'orange',
+    color: colors.white,
   },
-  restPos: {
-    backgroundColor: "gray"
+  circle: {
+    width: 30,
+    height: 30,
+    borderRadius: 15,
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginRight: 10,
   },
-  idCell:{
-    flex: 1,
-    fontSize: 16, 
-    fontWeight: 800
+  circleText: {
+    fontWeight: 'bold',
   },
   cell: {
-    flex:2,
-    fontSize: 16, 
-    fontWeight: 800
+    flex: 2,
+    fontSize: 16,
+    fontWeight: 'bold',
+    textAlign: 'center',
   },
-
 });
 
-
-export default LeaderboardScreen
+export default LeaderboardScreen;
