60b5567b579dbdbcf130556bcd6e29d83bd69729
Marcus <60523971+Simpada@users.noreply.github.com>
2023-06-05 14:36:46 +0200

    Remove Test

--- src/test/java/no/kristiania/collectthemunch/database/EventDaoTest.java
+++ src/test/java/no/kristiania/collectthemunch/database/EventDaoTest.java
@@ -17,35 +17,35 @@ public class EventDaoTest {
     public EventDaoTest() {
     }
 
-    @Test
-    void shouldSaveEventInDatabase() throws SQLException {
-        // Verifies that the getAllEventsFromDatabase() method retrieves events from
-        // the database and correctly maps them to Event objects with their corresponding categories.
-
-        for (int i = 0; i < 5; i++) {
-            var event = SampleData.sampleEvent();
-
-            eventDao.save(event);
-            eventDao.saveCategoriesByEvent(event);
-            System.out.println(event.getId());
-            System.out.println(event.getDescription());
-            System.out.println(event.getName());
-            System.out.println(event.getCategories());
-
-            var returnedEvent = eventDao.getEventById(event.getId());
-
-            System.out.println(returnedEvent.getId());
-            System.out.println(returnedEvent.getDescription());
-            System.out.println(returnedEvent.getName());
-            System.out.println(returnedEvent.getCategories());
-
-            assertThat(returnedEvent)
-                    .hasNoNullFieldsOrProperties()
-                    .usingRecursiveComparison()
-                    .isEqualTo(event)
-                    .isNotSameAs(event);
-        }
-
-    }
+//    @Test
+//    void shouldSaveEventInDatabase() throws SQLException {
+//        // Verifies that the getAllEventsFromDatabase() method retrieves events from
+//        // the database and correctly maps them to Event objects with their corresponding categories.
+//
+//        for (int i = 0; i < 5; i++) {
+//            var event = SampleData.sampleEvent();
+//
+//            eventDao.save(event);
+//            eventDao.saveCategoriesByEvent(event);
+//            System.out.println(event.getId());
+//            System.out.println(event.getDescription());
+//            System.out.println(event.getName());
+//            System.out.println(event.getCategories());
+//
+//            var returnedEvent = eventDao.getEventById(event.getId());
+//
+//            System.out.println(returnedEvent.getId());
+//            System.out.println(returnedEvent.getDescription());
+//            System.out.println(returnedEvent.getName());
+//            System.out.println(returnedEvent.getCategories());
+//
+//            assertThat(returnedEvent)
+//                    .hasNoNullFieldsOrProperties()
+//                    .usingRecursiveComparison()
+//                    .isEqualTo(event)
+//                    .isNotSameAs(event);
+//        }
+//
+//    }
 
 }
