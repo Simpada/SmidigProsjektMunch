093d6eae3dc85e4f0dc2bf9812d23c34327189d0
SamuelEBG <95290084+SamuelEBG@users.noreply.github.com>
2023-06-05 15:13:21 +0200

    started implementing review app

--- src/main/java/no/kristiania/collectthemunch/database/ReviewAppDao.java
+++ src/main/java/no/kristiania/collectthemunch/database/ReviewAppDao.java
@@ -1,4 +1,18 @@
 package no.kristiania.collectthemunch.database;
 
-public class ReviewAppDao {
+import jakarta.inject.Inject;
+import no.kristiania.collectthemunch.entities.Review;
+
+import javax.sql.DataSource;
+
+public class ReviewAppDao extends AbstractDao{
+
+    @Inject
+    public ReviewAppDao(DataSource dataSource) {
+        super(dataSource);
+    }
+
+    public Review save() {
+
+    }
 }
--- src/main/java/no/kristiania/collectthemunch/endpoints/ReviewAppEndpoint.java
+++ src/main/java/no/kristiania/collectthemunch/endpoints/ReviewAppEndpoint.java
@@ -10,6 +10,12 @@ import java.util.List;
 @Path("/review/app")
 public class ReviewAppEndpoint extends ApiEndPoint {
 
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void createAppReview(){
+        reviewAppDao.save();
+    }
+
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public List<Review> getAllAppReviews() throws SQLException {
