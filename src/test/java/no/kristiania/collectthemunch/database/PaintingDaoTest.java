baad4da23c8f6e647ff06a24950d0fa6fd715d93
TobiasLiu1990 <tobias.liu@live.se>
2023-06-06 10:52:32 +0200

    Added sampledata for paintings

--- src/test/java/no/kristiania/collectthemunch/database/PaintingDaoTest.java
+++ src/test/java/no/kristiania/collectthemunch/database/PaintingDaoTest.java
@@ -1,6 +1,21 @@
 package no.kristiania.collectthemunch.database;
 
+import no.kristiania.collectthemunch.MemoryDataSource;
+import org.h2.jdbcx.JdbcDataSource;
+import org.junit.jupiter.api.Test;
+
 public class PaintingDaoTest {
+
+    private final JdbcDataSource dataSource = (JdbcDataSource) MemoryDataSource.createTestDataSource();
+    private final PaintingDao paintingDao = new PaintingDao(dataSource);
+
+
+    @Test
+    void shouldSaveAndRetrieveUser() {
+        
+    }
+
+
 
 
 
