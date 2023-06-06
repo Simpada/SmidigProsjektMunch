0cd015f8a7b202a6d6ee918711dd58b38d98552e
TobiasLiu1990 <tobias.liu@live.se>
2023-06-06 14:21:44 +0200

    Test to check paintings a user has collected

--- src/test/java/no/kristiania/collectthemunch/database/PaintingDaoTest.java
+++ src/test/java/no/kristiania/collectthemunch/database/PaintingDaoTest.java
@@ -53,16 +53,9 @@ public class PaintingDaoTest {
 
     @Test
     void shouldRetrieveAllFromUser() throws SQLException {
-        int numOfPaintings = 10;
-
-        for (int i = 0; i < numOfPaintings; i++) {
-            paintingDao.save(samplePainting());
-        }
-
         var p1 = paintingDao.retrieve(1);
         var p2 = paintingDao.retrieve(2);
         var p3 = paintingDao.retrieve(3);
-
 
         var user = sampleUser();
         userdao.save(user);
@@ -74,17 +67,26 @@ public class PaintingDaoTest {
 
         List<Painting> paintingsInventory = paintingDao.retrieveAllForUser(user.getUserId());
 
-
-
         assertEquals(3, paintingsInventory.size());
-        assertTrue(paintingsInventory.contains(p1));
-        assertTrue(paintingsInventory.contains(p2));
-        assertTrue(paintingsInventory.contains(p3));
 
 
+        assertThat(paintingsInventory.get(0))
+                .hasNoNullFieldsOrProperties()
+                .usingRecursiveComparison()
+                .isEqualTo(p1)
+                .isNotSameAs(p1);
 
+        assertThat(paintingsInventory.get(1))
+                .hasNoNullFieldsOrProperties()
+                .usingRecursiveComparison()
+                .isEqualTo(p2)
+                .isNotSameAs(p2);
 
+        assertThat(paintingsInventory.get(2))
+                .hasNoNullFieldsOrProperties()
+                .usingRecursiveComparison()
+                .isEqualTo(p3)
+                .isNotSameAs(p3);
     }
-
 
 }
