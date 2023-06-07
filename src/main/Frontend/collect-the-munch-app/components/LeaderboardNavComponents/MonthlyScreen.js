c1128106ea301e1fdcaa02ba34b30efd91baf120
Marcushansen91 <56343741+Marcushansen91@users.noreply.github.com>
2023-06-07 10:52:12 +0200

    LeaderboardNav

---
+++ src/main/Frontend/collect-the-munch-app/components/LeaderboardNavComponents/MonthlyScreen.js
@@ -0,0 +1,104 @@
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, FlatList, Button, ActivityIndicator } from 'react-native';
+import axios from 'axios';
+
+function MonthlyScreen() {
+  // Add more dummy data as needed to fill the page
+  const dummyData = [
+    { id: '1', username: 'User 1', points: 100 },
+    { id: '2', username: 'User 2', points: 90 },
+    { id: '3', username: 'User 3', points: 100 },
+    { id: '4', username: 'User 4', points: 90 },
+    { id: '5', username: 'User 5', points: 100 },
+    { id: '6', username: 'User 6', points: 90 },
+    { id: '7', username: 'User 7', points: 100 },
+    { id: '8', username: 'User 8', points: 90 },
+    { id: '9', username: 'User 9', points: 100 },
+    { id: '10', username: 'User 10', points: 90 },
+    { id: '11', username: 'User 11', points: 100 },
+    { id: '12', username: 'User 12', points: 90 },
+    { id: '50', username: 'User 50', points: 1 },
+  ];
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 10;
+  const [isLoading, setIsLoading] = useState(false);
+
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = dummyData.slice(indexOfFirstItem, indexOfLastItem);
+
+  useEffect(() => {
+    setIsLoading(true);
+    // Uncomment this block when your API is ready
+    /*
+    axios.get(`https://your-api-url/monthly?page=${currentPage}`)
+      .then(response => {
+        setData(response.data);
+        setIsLoading(false);
+      })
+      .catch(error => {
+        console.error(error);
+        setIsLoading(false);
+      });
+    */
+    setIsLoading(false);
+  }, [currentPage]);
+
+  return (
+    <View style={styles.container}>
+      <Text>Monthly Leaderboard</Text>
+      {isLoading ? (
+        <ActivityIndicator size="large" color="#0000ff" />
+      ) : (
+        <FlatList
+          data={currentItems}
+          keyExtractor={item => item.id.toString()}
+          renderItem={({ item, index }) => (
+            <View style={styles.item}>
+              <Text>{index + 1}</Text>
+              <Text>{item.username}</Text>
+              <Text>{item.points} points</Text>
+            </View>
+          )}
+        />
+      )}
+      <View style={styles.pagination}>
+        <Button
+          title="Previous"
+          onPress={() => setCurrentPage(Math.max(1, currentPage - 1))}
+          disabled={currentPage === 1}
+        />
+        <Button
+          title="Next"
+          onPress={() => setCurrentPage(currentPage + 1)}
+          disabled={currentPage * itemsPerPage >= dummyData.length}
+        />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: 20,
+  },
+  item: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: 10,
+    borderBottomColor: '#ccc',
+    borderBottomWidth: 1,
+  },
+  pagination: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 10,
+  },
+});
+
+export default MonthlyScreen;
+
--- src/main/Frontend/collect-the-munch-app/screens/LeaderboardScreen.js
+++ src/main/Frontend/collect-the-munch-app/screens/LeaderboardScreen.js
@@ -1,119 +1,20 @@
-import { useEffect, useState } from 'react';
-import { Text, View, StyleSheet, ScrollView } from 'react-native'
-import { colors } from '../Styles/theme';
-import Header from '../components/Header';
-const LeaderboardScreen = () => {
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import WeeklyScreen from '../components/LeaderboardNavComponents/WeeklyScreen';
+import MonthlyScreen from '../components/LeaderboardNavComponents/MonthlyScreen';
+import AllTimeScreen from '../components/LeaderboardNavComponents/AllTimeScreen';
 
-  const [leaderboard, setLeaderBoard] = useState([])
+const Tab = createMaterialTopTabNavigator();
 
-  useEffect(() => {
-      const initialLeaderboard = [
-        {id: 1, fullName:"Jonas Andersen", userName: "Jønna", points: 2000 },
-        {id: 2, fullName:"Sander Sanstrød", userName: "Sand", points: 1800 },
-        {id: 3, fullName:"Samuel something", userName: "The Cheif", points: 1500 },
-        {id: 4, fullName:"Samuel something", userName: "The Cheif", points: 1500 },
-        {id: 5, fullName:"Samuel something", userName: "The Cheif", points: 1500 },
-        {id: 6, fullName:"Samuel something", userName: "The Cheif", points: 1500 },
-      ]
-
-      setLeaderBoard(initialLeaderboard)
-  }, [])
-
-
-
-    return  (
-      <View style={styles.container}>
-       <ScrollView>
-      <View style={styles.table}>
-        <View style={styles.tableHeader}>
-          <Text style={styles.headerIdText}>ID</Text>
-          <Text style={styles.headerText}>Full Name</Text>
-          <Text style={styles.headerText}>Name</Text>
-          <Text style={styles.headerText}>Points</Text>
-        </View>
-        {leaderboard.map((item, index) => (
-          <View
-            style={
-              [
-                styles.tableRow, index <= 0 && styles.topOneRow,
-                styles.tableRow, index <= 1 && index >=1 && styles.topTwoRow,
-                styles.tableRow, index <= 2 && index >=2 && styles.topThreeRow,
-                styles.tableRow, index > 2 && styles.restPos,
-
-              ]
-            }
-            key={item.id}
-          >
-            <Text style={styles.idCell}>{item.id}</Text>
-            <Text style={styles.cell}>{item.fullName}</Text>
-            <Text style={styles.cell}>{item.userName}</Text>
-            <Text style={styles.cell}>{item.points}</Text>
-          </View>
-        ))}
-      </View>
-    </ScrollView>
-    </View>
+function LeaderboardScreen() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Weekly" component={WeeklyScreen} />
+      <Tab.Screen name="Monthly" component={MonthlyScreen} />
+      <Tab.Screen name="All Time" component={AllTimeScreen} />
+    </Tab.Navigator>
   );
-};
+}
 
-const styles = StyleSheet.create({
-  container: {
-    marginTop: 100
-  },
-  table: {
-    borderWidth: 1,
-    borderColor: 'black',
-    margin: 10,
-  },
-  tableHeader: {
-    flexDirection: 'row',
-    backgroundColor: colors.navy,
-    padding: 10,
-  },
-  headerIdText:{
-    flex:1,
-    color: colors.white,
-    fontWeight: 'bold',
-  },
-  headerText: {
-    flex: 2,
-    fontSize: 18,
-    color: colors.white,
-    fontWeight: 'bold',
-  },
-  tableRow: {
-    flexDirection: 'row',
-    borderBottomWidth: 1,
-    borderColor: 'gray',
-    padding: 10,
-  },
-  topOneRow: {
-    backgroundColor: "green",
-    color: colors.white 
-  },
-  topTwoRow: {
-    backgroundColor: "yellow",
-    color: colors.white 
-  },
-  topThreeRow:{
-    backgroundColor: "orange",
-    color: colors.white 
-  },
-  restPos: {
-    backgroundColor: "gray"
-  },
-  idCell:{
-    flex: 1,
-    fontSize: 16, 
-    fontWeight: 800
-  },
-  cell: {
-    flex:2,
-    fontSize: 16, 
-    fontWeight: 800
-  },
-
-});
-
-
-export default LeaderboardScreen
+export default LeaderboardScreen;
